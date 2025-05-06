21969a7f38ec34b9bcc8cf50bb681e73bbf57ea9
Bea <113467080+GitxGot@users.noreply.github.com>
2025-05-06 15:19:41 -0400

    trying to figure out manage

--- manage.py
+++ manage.py
@@ -1,17 +1,23 @@
-from flask import Flask
-from flask_script import Manager
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+
 import os
-import psycopg2
-from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
-app = Flask(__name__)
-# Database connection
-url = os.environ.get("DATABASE_URL")
-connection = psycopg2.connect(url)
-manager = Manager(app)
-@manager.command
-def run():
-    """Run the Flask application."""
-    app.run()
-if __name__ == '__main__':
-    manager.run()
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
