328631297cd0c5356cc79cafb9455dbac3873e2d
Bea <113467080+GitxGot@users.noreply.github.com>
2025-05-06 16:46:32 -0400

    revert manage

--- manage.py
+++ manage.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-
+from flask import Flask
+from flask_script import Manager
 import os
-import sys
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+app = Flask(__name__)
+# Database connection
+url = os.environ.get("DATABASE_URL")
+connection = psycopg2.connect(url)
+manager = Manager(app)
+@manager.command
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
+def run():
+    """Run the Flask application."""
+    app.run()
+if __name__ == '__main__':
+    manager.run()
