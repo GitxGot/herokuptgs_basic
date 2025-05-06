f5af9a70ed0e8a62149800debeb363bf8392f7d3
Bea <113467080+GitxGot@users.noreply.github.com>
2025-05-06 17:09:08 -0400

    settings:  change for psql db imrpove static consts

--- herokuptgs_basic/settings.py
+++ herokuptgs_basic/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'herokuptgs_basic.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "flask_db",
+        "USER": "god",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -115,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 # Default primary key field type
